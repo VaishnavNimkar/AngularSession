888326cb3badd6da5f51c675a4a1cecda8d73b0f
Vaishnav Vijay Nimkar <VaishnavVijay.Nimkar@philips.com>
2022-09-28 16:19:39 +0530

    adding hostbinding for border

--- Day5/src/app/directives/textColor.directive.ts
+++ Day5/src/app/directives/textColor.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, Renderer2, Input, OnInit, OnChanges, SimpleChanges, HostListener } from "@angular/core";
+import { style } from "@angular/animations";
+import { Directive, ElementRef, Renderer2, Input, OnInit, OnChanges, SimpleChanges, HostListener, HostBinding } from "@angular/core";
 
 @Directive({
     selector:'[textColor]'
@@ -9,9 +10,12 @@ export class ColorDirective implements OnInit, OnChanges
 
     @Input() textColor= "";
 
+    @HostBinding('style.border') border:string = "";
+
     constructor(private targetElement:ElementRef, private domApi:Renderer2)
     {
         this.domApi.setStyle(this.targetElement.nativeElement, "color", this.textColor || "red");
+        this.border = "dotted 2px";
     }
     
     ngOnChanges(changes: SimpleChanges): void {
@@ -29,11 +33,13 @@ export class ColorDirective implements OnInit, OnChanges
     onMouseOverOnHostElement()
     {
         this.domApi.setStyle(this.targetElement.nativeElement, "color", "green");
+        this.border = "solid 4px";
     }
 
     @HostListener("mouseleave")
     onMouseLeaveFromHostElement()
     {
         this.domApi.setStyle(this.targetElement.nativeElement, "color", this.textColor || "red");
+        this.border = "dotted 2px";
     }
 }
